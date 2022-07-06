ba948e9c758cc0e409ff796501697f3cef685c17
unknown <grisha_kirillov@mail.ru>
2022-07-07 01:16:28 +0300

    initial

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,7 @@ import reactDom from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-const root = reactDom.createRoot(
-    document.getElementById('root') as HTMLElement,
-);
+const root = reactDom.createRoot(document.getElementById('root'));
 
 root.render(
     <BrowserRouter>
